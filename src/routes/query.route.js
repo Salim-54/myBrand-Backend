3ee1d3c98f18b0bff0062a5e15e70d6eb3702cd7
NDAYISABYE Salim <ndayisabyesalim54@gmail.com>
2022-03-04 20:41:58 +0200

    CRUD operations for blogs initiated create blog edit blog load blog load blogs delete blog

--- src/routes/query.route.js
+++ src/routes/query.route.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import { getAllQueries, saveQuery } from '../controllers/query.controller';
+import { getAllQueries, saveQuery, getQueryById } from '../controllers/query.controller';
 
 const router = express.Router();
 
 router.post('/', saveQuery);
 router.get('/', getAllQueries);
+router.get('/:id', getQueryById);
 
 
 export default router;
+
+
+
