0d64399f6f6ec6bc9f442ab8ff5887b6f4e98cf2
NDAYISABYE Salim <ndayisabyesalim54@gmail.com>
2022-03-07 22:31:42 +0200

    chore(handling-errors) json formated error handlers

--- src/app.js
+++ src/app.js
@@ -1,23 +1,44 @@
 import "./database";
+import 'dotenv/config';
+
+import AppError from "./utils/appError";
+import globalErrorHandler from "./controllers/error.controller";
 import express from 'express';
 import queriesRoutes from './routes/query.route';
 import blogsRoutes from './routes/blog.route';
+import userRoutes from './routes/user.route';
+
+import res from "express/lib/response";
 
 
 
 const server = express();
 
-// default route
 server.get('/', (req, res) => {
     res.status(200).json({success: true, message: "You successfully on salim's app API"})
 });
 
-server.use(express.json());
-
-server.use('/api/v1/queries', queriesRoutes);
-server.use('/api/v1/blogs', blogsRoutes);
-
-
 const port = 3000;
 
 server.listen(port, () => {console.log("Server listening on port "+port)});
+
+
+
+server.use(express.json());
+
+server.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+server.use('/api/v1/queries', queriesRoutes);
+server.use('/api/v1/blogs', blogsRoutes);
+server.use('/api/v1/users', userRoutes);
+
+
+server.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+  server.use(globalErrorHandler);
+ 
