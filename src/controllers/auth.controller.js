dc1f3657dd6ab5d2f66b3434206f9030c399ebf9
NDAYISABYE Salim <ndayisabyesalim54@gmail.com>
2022-03-07 22:58:21 +0200

    feature(loging in a user)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -36,4 +36,67 @@ exports.signup = catchAsync(async (req, res, next) => {
     })
 });
 
+exports.login = catchAsync(async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    //  Check if email and password exist
+    if (!email || !password) {
+     return   next(new AppError('Please provide email and password!', 400));
+    }
+    //  Check if user exists && password is correct
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new AppError('Incorrect email or password', 401));
+    }
+  
+    // // If everything ok, send token to client
+    // createSendToken(user, 200, res);
+
+    const token = signToken(user._id);
+    res.status(200).json({
+        status: 'success',
+        token
+    });
+
+  });
+
+
+  exports.protect = catchAsync(async (req, res, next) => {
+    // Getting token and check if it's there
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+      token = req.headers.authorization.split(' ')[1];
+    //   console.log(token);
+    }
+  
+    if (!token) {
+      return next(
+        new AppError('You are not logged in! Please log in to get access.', 401)
+      );
+    }
+  
+    // //  Verifying the token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+    // console.log(decoded);
+
+    // // 3) Check if user still exists
+
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(
+        new AppError(
+          'The user belonging to this token does no longer exist.',
+          401
+        )
+      );
+    }
+  
+    // // GRANT ACCESS TO PROTECTED ROUTE
+    req.user = currentUser;
+    next();
+  });  
+  
+ 
+
 
